632a2018f7c761cc5a17ceb86852cfbc80fa1e34
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-27 18:11:54 +0300

    forgotten statement deleted.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var router = express.Router();
 
-router.get('/', urlencodedParser, (req, res) => {
+router.get('/', (req, res) => {
     console.log(req.headers);
     console.log(req.connection.remoteAddress);
     res.send(req.connection.remoteAddress);
