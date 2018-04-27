3a92829db8f6bfb91c90a35ad80b6940bf1d3951
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-27 18:47:22 +0300

    language and software is working, testing ip.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,12 +1,27 @@
 const express = require('express');
 var router = express.Router();
 
+function parseLanguage(acceptLanguage) {
+    let comma = acceptLanguage.indexOf(',');
+    return acceptLanguage.split("").slice(0, comma).join("");
+}
+
+function parseUserAgent(userAgent) {
+    let leftParanthese = userAgent.indexOf('(');
+    let rightParanthese = userAgent.indexOf(')');
+    return userAgent.split("").slice(leftParanthese + 1, rightParanthese).join("");
+}
+
 router.get('/', (req, res) => {
-    let ip = req.ip;
-    let ipDetails = req.socket.address();
-    console.log(req.headers);
-    console.log(req.socket.address());
-    res.send(ipDetails);
+    let ip = req.headers['x-forwarded-for'] || req.ip;
+    let language = parseLanguage(req.headers["accept-language"]);
+    let software = parseUserAgent(req.headers["user-agent"]);
+    let result = {
+        "ipaddress": ip,
+        "language": language,
+        "software": software
+    };
+    res.json(result);
 });
 
 module.exports = router;
