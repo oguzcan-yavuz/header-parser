684e1bebf6b2c2c3ef97e40de6aa9ac4d675d288
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-27 18:10:22 +0300

    body-parser is seem like unnecessary, trying to get users IP address.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("yey!");
+router.get('/', urlencodedParser, (req, res) => {
+    console.log(req.headers);
+    console.log(req.connection.remoteAddress);
+    res.send(req.connection.remoteAddress);
 });
 
 module.exports = router;
