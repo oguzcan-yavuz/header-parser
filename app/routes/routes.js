852d611adb6d891beb1e73f7461df57ba7f8e85d
Oğuzcan Yavuz <oguzcanyavuz321@gmail.com>
2018-04-27 18:17:41 +0300

    ip address tryouts

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -2,9 +2,11 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
+    let ip = req.ip;
+    let ipDetails = req.socket.address();
     console.log(req.headers);
-    console.log(req.connection.remoteAddress);
-    res.send(req.connection.remoteAddress);
+    console.log(req.socket.address());
+    res.send(ipDetails);
 });
 
 module.exports = router;
